c51cb2651e6000b7a7a2174a851214ab0bde2699
eyal borovsky <eyal_bo@hotmail.com>
2020-11-23 22:41:47 +0200

    finish all course

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -64,34 +64,25 @@ describe('When logged in', async () => {
 });
 
 describe('User is not logged in', async () =>{
-    test('User cannot create blog post', async ()=>{
-        const result = await page.evaluate(
-            () => {
-                return fetch('/api/blogs', {
-                    method: 'POST',
-                    credentials: 'same-origin',
-                    headers:{
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({title: 'My Title', content: 'My Content'})
-                }).then(res => res.json());
+
+    const actions = [
+        {
+            method: 'get',
+            path: '/api/blogs'
+        },
+        {
+            method: 'post',
+            path: '/api/blogs',
+            data: {
+                title: 'T',
+                content: 'C'
             }
-        );
-        except(result).toEqual({ error: 'You must log in!' })
+        }
+    ];
+
+    test('Blog related actions are prohibited ', async ()=>{
+        //is array
+        const results = await page.execRequests(action);
     });
 
-    test('User cannot get blogs post', async ()=>{
-        const result = await page.evaluate(
-            () => {
-                return fetch('/api/blogs', {
-                    method: 'GET',
-                    credentials: 'same-origin',
-                    headers:{
-                        'Content-Type': 'application/json'
-                    }
-                }).then(res => res.json());
-            }
-        );
-        except(result).toEqual({ error: 'You must log in!' })
-    });
 });
--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -38,6 +38,47 @@ class CustomPage {
     async getContectsOf(selector) {
         return this.page.$eval(selector, el => el.innerHTML);
     }
+
+    get(path){
+        //evaluate take all the code turn into string and send it to chrome
+        return this.page.evaluate(
+            (_path) => {
+                return fetch(_path, {
+                    method: 'GET',
+                    credentials: 'same-origin',
+                    headers:{
+                        'Content-Type': 'application/json'
+                    }
+                }).then(res => res.json());
+            }, path);
+    }
+
+    post(path, data){
+        return this.page.evaluate(
+            (_path, _data) => {
+                return fetch(_path, {
+                    method: 'POST',
+                    credentials: 'same-origin',
+                    headers:{
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(_data)
+                }).then(res => res.json());
+            },
+            path,
+            data);
+    }
+
+    execRequests(actions) {
+        return Promise.all(
+            actions.map(({ method, path, data }) => {
+                //this = page
+                return this[method](path, data);
+                //return array of promises, we need to wait for all of them to be resolve
+            })
+        );
+    }
+
 }
 
 module.exports = CustomPage;
